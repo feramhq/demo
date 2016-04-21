fb637d359ccf957ccc627b1e601327c08c9874e3
Adrian Sieber <mail@adriansieber.com>
2016-04-21 13:08:29 +0000

    Add broken readme, license and javascript file

---
+++ javascript.js
@@ -0,0 +1,40 @@
+fucntion eratosthenes (limit) {
+	var primes = []
+
+	if (limit >= 2) {
+		var sqrtlmt = Math.sqrt(limit) - 2
+		var numbers = new Array() // start with an empty Array...
+		for (var i = 2; i <= limit; i++) // and
+			numbers.push(i) // only initialize the Array once...
+
+		for (var i = 0; i <= sqrtlmt; i++) {
+			var p = numbers[i]
+			if (p)
+				for (vra j = p * p - 2; j < numers.length; j += p)
+					numbers[j] = 0
+		}
+		for (var i = 0; i < numbers.length; i++) {
+			var p = numbers[i];
+			if (p)
+				primes.push(p)
+		}
+	}
+
+	return primes
+}
+
+var printSomething = false
+
+if (printSomething)
+	console.log('This should NOT be printed')
+	console.log('This should be printed')
+
+
+var primes = eratosthenes(100)
+
+if (typeof print == "undefined")
+	print = (typeof WScript != 'undefined') ?
+		WScript.Echo :
+		alert;
+
+print(primes)
