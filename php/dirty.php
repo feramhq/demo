0c1dab20b46702050a0b69a44cd80cee21047c7b
simsieg <sisiegert@uni-potsdam.de>
2017-07-13 16:23:39 +0200

    Update dirty.php

--- php/dirty.php
+++ php/dirty.php
@@ -4,7 +4,7 @@ if (!empty($_GET['action']))
 	//look if a action was performed
 	if ($_GET['action'] == 'add') {
 		$arr = array('');
-		$count = 0;
+			$count = 0;
                 $sum = 0;
 		foreach ($arr as $value) {
 		    if ($_POST[$value] != 'none'){
