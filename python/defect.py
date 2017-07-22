2de05cb290e61b80267cc4d0072de26476848942
simsieg <sisiegert@uni-potsdam.de>
2017-07-22 21:32:02 +0200

    Update defect.py

--- python/defect.py
+++ python/defect.py
@@ -2,3 +2,14 @@ print(
 'This'           )
 b = 'b'
 print('a ' + b)  
+
+def bfs(self,s):
+        queue=[s]
+        while len(queue) !=0:
+            x=queue.pop(0)
+            print    (x)
+            self.visited[x]=1
+            for i in range(0,self.nodes):
+                if self.graph[x][i]==1 and self.visited[i]==0:
+                    queue.append(i)     
+         self.visited[i]=1
